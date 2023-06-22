4148f822e0b1686fe31370ed078a849f7fbf6fb1
Reydel Ocon <reydel321@gmail.com>
2023-06-22 22:27:36 +0800

    hehehe

--- admin/functions/delete-account.php
+++ admin/functions/delete-account.php
@@ -1,33 +1,31 @@
 <?php
-// Assuming you have a database connection established
 
-// Database credentials
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "seait-students";
 
-// Create a database connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if the form is submitted
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if the delete button is clicked
+
     if (isset($_POST['delete-id'])) {
-        // Get the account ID to delete
+
         $accountId = $_POST['delete-id'];
 
-        // Delete the account from the database
+
         $deleteSql = "DELETE FROM accounts WHERE id = '$accountId'";
         if ($conn->query($deleteSql) === TRUE) {
             echo "Account deleted successfully.";
 
-            // Redirect back to the previous page
+
             header("Location: ../manage-account.php");
             exit();
         } else {
@@ -36,5 +34,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-// Close the database connection
+
 $conn->close();
